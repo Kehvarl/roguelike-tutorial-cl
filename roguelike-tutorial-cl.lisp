1d85fccb903bb3e821f1032333289e8bb4b57b25
Kehvarl <kehvarl@kehvarl.com>
2021-09-27 18:27:07 -0700

    Opening a blank Window

--- roguelike-tutorial-cl.lisp
+++ roguelike-tutorial-cl.lisp
@@ -5,15 +5,21 @@
 (defparameter *screen-width* 80)
 (defparameter *screen-height* 50)
 
+;; Draw our terminal window
 (defun draw()
   (blt:clear)
   (blt:refresh))
 
+;; Terminal Window Settings.
 (defun config ()
   (blt:set "window.resizeable = true")
   (blt:set "window.size = ~Ax~A" *screen-width* *screen-height*)
   (blt:set "window.title = Common Lisp Roguelike Tutorial"))
 
+;; Create a terminal window using the settings in our `config` function.
+;; Then enter a loop where we draw the screen and check for a keypress
+;; If the user closes the window or presses Escape, we exit.
+;; Otherwise loop forever
 (defun main()
   (blt:with-terminal
     (config)
