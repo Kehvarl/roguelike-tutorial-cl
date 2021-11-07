46cf0665d3edb0e94f5d67198d1076c1d5152f0a
Kehvarl <kehvarl@kehvarl.com>
2021-11-06 19:04:07 -0700

    Update Game-Tick for states

--- roguelike-tutorial-cl.lisp
+++ roguelike-tutorial-cl.lisp
@@ -72,13 +72,14 @@
 ;; Check for any player inputs
 ;; Perform any needed actions
 ;; Return to our main loop.  Pass along the current value of `exit`
-(defun game-tick (player entities map)
+(defun game-tick (player entities map game-state)
+  (declare (type game-states game-state))
   (render-all entities map)
   (let* ((action (handle-keys))
          (move (getf action :move))
          (exit (getf action :quit)))
 
-    (when move
+    (when (and move (eql game-state :player-turn))
       (let ((destination-x (+ (entity/x player) (car move)))
             (destination-y (+ (entity/y player) (cdr move))))
         (unless (blocked-p map destination-x destination-y)
@@ -87,8 +88,20 @@
                    (format t "~A kicks the ~A.~%" (entity/name player) (entity/name target)))
                   (t
                    (move player (car move) (cdr move))
-                   (fov map (entity/x player) (entity/y player))))))))
-    exit))
+                   (fov map (entity/x player) (entity/y player))))
+            (setf game-state :enemy-turn))))
+      (when exit
+        (setf game-state :exit)))
+    
+    (when (eql game-state :enemy-turn)
+      (dolist (entity entities)
+        (if (not (eql player entity))
+            (format t "The ~A sits idly.~%" (entity/name entity))))
+      (setf game-state :player-turn)))
+
+  game-state)
+
+
 
 ;; Create a terminal window using the settings in our `config` function.
 ;; Then enter a loop where we let our game-tick perform its duties.
